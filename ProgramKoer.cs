d2cbba5914674cd91682af0a4ad4e346abad3394
opilane <opilane@HP-CZC0359NP5>
2025-01-17 10:51:22 +0200

    Add project files.

---
+++ ProgramKoer.cs
@@ -0,0 +1,55 @@
+﻿namespace M_koer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisestage esimene arv!");
+            int esimeneArv = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisestage teine arv!");
+            int teineArv = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Sisestage tehe (+, -, *, /): ");
+            string tehe = Console.ReadLine();
+
+            double tulemus = 0;
+
+            if (tehe == "+")
+            {
+                tulemus = esimeneArv + teineArv;
+            }
+            else if (tehe == "-")
+            {
+                tulemus = esimeneArv - teineArv;
+            }
+            else if (tehe == "*") 
+            {
+                tulemus = esimeneArv * teineArv;
+            }
+            else if (tehe == "/")
+            {
+                tulemus = esimeneArv / teineArv;
+            }
+            else
+            {
+                Console.WriteLine("Tulemus: " + tulemus);
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
